87b73be1e518b565f23f1fc0b2a499b55242e2c7
Kyle Chriscoe <kyle717@gmail.com>
2021-01-19 15:38:34 -0500

    added route for GET index.html

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+// ===SERVER SETUP===
+// ---Importing required packages---
 const express = require("express");
+const path = require("path");
 
+// ---Initialize Express app---
 let app = express();
 const PORT = 3000;
 
-app.get("/", function(req, res) {
-    res.send("Welcome to the Note Taker");
+// ---Adding Middleware for data parsing---
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// ===ROUTES===
+// ---Basic route to main page---
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 })
 
 app.listen(PORT, function() {
