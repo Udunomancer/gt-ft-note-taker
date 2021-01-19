0efb53fcc62e85f9e59c8198cc030b525026ceb1
Kyle Chriscoe <kyle717@gmail.com>
2021-01-19 18:17:01 -0500

    linking static files to served html

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // ---Importing required packages---
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
 // ---Initialize Express app---
 let app = express();
@@ -10,17 +11,11 @@ const PORT = process.env.PORT || 8080;
 // ---Adding Middleware for data parsing---
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
 
 // ===ROUTES===
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
-// ===API ROUTES===
-// ---GET all note objects---
-// app.get("/api/notes", function(req, res) {
-//     let allNotes = fs.readFile(path.join(__dirname, "db/db.json"));
-//     return res.json
-// })
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
